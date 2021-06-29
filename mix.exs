b23d324795304d70b71a3f6004fc926b217b746b
Jeff Suttor <jeff.suttor@earthlink.net>
2021-06-28 20:53:30 -0700

    update HelloWorld to mix new --sup

--- mix.exs
+++ mix.exs
@@ -7,14 +7,17 @@ defmodule HelloWorld.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      preferred_cli_env: [testy: :test]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {HelloWorld.Application, []}
     ]
   end
 
@@ -24,6 +27,27 @@ defmodule HelloWorld.MixProject do
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.24", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: [
+        "deps.clean --unused --unlock",
+        "deps.get",
+        "deps.compile"
+      ],
+      qa: [
+        "format --check-formatted",
+        "compile --warnings-as-errors",
+        "credo --strict",
+        "dialyzer",
+        "docs"
+      ],
+      testy: [
+        "test --warnings-as-errors --cover --export-coverage default",
+        "test.coverage"
+      ]
     ]
   end
 end
